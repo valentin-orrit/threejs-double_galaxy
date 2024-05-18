8f574aa72c0ccb6fa96bcc84647ca5f008edac8d
valentin-orrit <vorrit@gmail.com>
2024-05-18 17:15:05 +0200

    removed warning

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,8 @@ export default {
     {
         outDir: '../dist', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
+        sourcemap: true, // Add sourcemap
+        CI: false
+
     },
 }
